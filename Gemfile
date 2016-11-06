60e02d4b000629ed02324fd1a7251800ea5c98c3
Lepkele <charlessport2000@yahoo.com>
2016-11-06 05:34:41 +0000

    Complete artifacts

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '4.2.5'
 # Use SCSS for stylesheets
 gem 'devise'
 gem 'milia'
-
+gem 'aws-sdk'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
-    
-    ALERT_TYPES = [:success, :info, :warning, :danger] unless const_defined?(:ALERT_TYPES)
+  ALERT_TYPES = [:success, :info, :warning, :danger] unless const_defined?(:ALERT_TYPES)
 
   def bootstrap_flash(options = {})
     flash_messages = []
@@ -19,7 +18,8 @@ module ApplicationHelper
         class: "alert fade in alert-#{type} #{tag_class}"
       }.merge(options)
 
-      close_button = content_tag(:button, raw("&times;"), type: "button", class: "close", "data-dismiss" => "alert")
+      close_button = content_tag(:button, raw("&times;"), type: "button", class: "close", 
+        "data-dismiss" => "alert")
 
       Array(message).each do |msg|
         text = content_tag(:div, close_button + msg, tag_options)
@@ -28,10 +28,13 @@ module ApplicationHelper
     end
     flash_messages.join("\n").html_safe
   end
-end
-    
-
+  
   def tenant_name(tenant_id)
     Tenant.find(tenant_id).name
   end
-
+  
+  def s3_link(tenant_id, artifact_key)
+    link_to artifact_key, "#{artifact_key}", class: "main-link", target: 'new'
+  end
+  
+end
